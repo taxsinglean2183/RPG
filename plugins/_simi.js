84e8e9ee7bf1493dced94e03eacc925ba6e00d9a
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-02-08 20:38:25 +0700

    p

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -6,11 +6,11 @@ handler.before = async (m) => {
     if (chat.simi && !chat.isBanned && !m.isGroup) {
         if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let res = await fetch(`https://api.xteam.xyz/simsimi?kata=${text}&APIKEY=KingOfBear`)
+        let res = await fetch(`https://apikey-bear3.herokuapp.com/api/f/simi2?apikey=${bearkey}&query=${text}`)
         if (!res.ok) return m.reply(eror)
         let json = await res.json()
-        if (json.jawaban == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
-        else await m.reply(`*Simi:* ${json.jawaban}`)
+        if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
+        else await m.reply(`*Simi:* ${json.result}`)
         return !0
     }
     return true
