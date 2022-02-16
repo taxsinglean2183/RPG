1d005339dffc6a4bf87245e712b07b291d809a34
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-02-16 10:38:24 +0700

    p

--- plugins/magernulis.js
+++ plugins/magernulis.js
@@ -1,15 +1,12 @@
-// Magernulis By MFarelS:V
-// Code by DrawlNag
-// Recode by Nurutomo :V
-let handler  = async (m, { command, conn, text }) => {
-m.reply(wait)
-  let id = (command.match(/[1-6]$/) || [])[0] || ''
-  await conn.sendFile(m.chat, global.API('xteam', '/magernulis' + id, {
-    text,
-    nama: conn.getName(m.sender),
-    kelas: ' '
-  }, 'APIKEY'), 'nulis.jpg', 'Nahhh sudah jadi...', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
-
+let fetch = require('node-fetch')
+let handler = async (m, { command, conn, args, usedPrefix }) => {
+  if (!args[0]) throw `contoh:\n${usedPrefix + command} Bear Ganteng`
+  m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
+  let res = await fetch(`https://apikey-bear3.herokuapp.com/api/maker/nulis?apikey=${bearkey}&text=${args[0]}`)
+  if (!res.ok) throw eror
+  let json = await res.json()
+  if (json.status != true) throw json
+conn.sendFile(m.chat, json.result, 'eror.jpg', `Ni kak udah jadi hehe:v\n${footer}`, m, 0, { thumbnail: await (await fetch(json.result)).buffer() })
 }
 handler.help = new Array(6).fill('magernulis').map((v, i) => v + (i + 1) + ' <teks>')
 handler.tags = ['nulis']
