4dec617d482b4af10d6771e8c34f22c776c128f2
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-02-16 10:48:41 +0700

    p

--- plugins/ttstalk.js
+++ plugins/ttstalk.js
@@ -1,12 +1,22 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `contoh:\n${usedPrefix + command} raraharsita2`
-
-  let res = await fetch(`https://api.lolhuman.xyz/api/stalktiktok/${args[0]}?apikey=${lolkey}`)
+  m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
+  let res = await fetch(`https://apikey-bear3.herokuapp.com/api/stalk/tiktok?apikey=${bearkey}&username=${args[0]}`)
   if (!res.ok) throw eror
   let json = await res.json()
-  if (json.status != 200) throw json
-  conn.sendFile(m.chat, json.result.user_picture, 'eror.jpg', `*Nama:* ${json.result.username}\n*Bio:* ${json.result.bio}\n*Followers:* ${json.result.followers} Followers\n*Following:* ${json.result.followings} Following\n*Posts:* ${json.result.video} Posts\n*Link:* https://www.tiktok.com/@${json.result.username}\n\n_*~SUPPORT TERUS KING OF BEAR*_`, m, 0, { thumbnail: await (await fetch(json.result.user_picture)).buffer() })
+  if (json.status != true) throw json
+   let iggs = `
+┏━━━ꕥ *TIKTOK STALKER* ꕥ━⬣
+┃✾ *Nama:* ${json.result.username}
+┃✾ *Bio:* ${json.result.bio}
+┃✾ *Followers:* ${json.result.followers} Followers
+┃✾ *Following:* ${json.result.followings} Following
+┃✾ *Posting:* ${json.result.video} Postingan
+┃✾ *Link:* https://www.tiktok.com/@${json.result.username}
+┗━❑
+${footer}`.trim()
+  conn.sendFile(m.chat, json.result.user_picture, 'eror.jpg', iggs, m, 0, { thumbnail: await (await fetch(json.result.user_picture)).buffer() })
 }
 
 handler.help = ['ttstalk <username>']
