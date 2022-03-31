a520c7645699458c3c9eab41288822f67e0b46f8
taxsinglean2183 <102736242+taxsinglean2183@users.noreply.github.com>
2022-03-31 19:08:48 +0700

    p

--- plugins/ppcouple.js
+++ plugins/ppcouple.js
@@ -4,12 +4,12 @@ let handler = async (m, { conn }) => {
 //https://api.lolhuman.xyz/api/random/ppcouple?apikey=RFK-Rey
 m.reply(wait)
 
-let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${lolkey}`)
+let res = await fetch(`https://saipulanuar.herokuapp.com/api/random/couple?apikey=${bearkey}`)
 let json = await res.json()
 
-conn.sendFile(m.chat, json.result.male, 'ppcowo.png', 'Cowok', m, false,{ contextInfo: { forwardingScore: 999, isForwarded: true }})
+conn.sendFile(m.chat, json.result.ppcwo, 'ppcowo.png', 'Cowok', m, false,{ contextInfo: { forwardingScore: 999, isForwarded: true }})
 
-conn.sendFile(m.chat, json.result.female, 'ppcewe.png', 'Cewek', m,false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+conn.sendFile(m.chat, json.result.ppcwe, 'ppcewe.png', 'Cewek', m,false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
 }
 handler.help = ['ppcouple', 'ppcp']
