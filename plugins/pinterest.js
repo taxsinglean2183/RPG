a06eaf0c1bb086365ec66149883eb75f88c3f3f3
taxsinglean2183 <102736242+taxsinglean2183@users.noreply.github.com>
2022-03-31 19:06:58 +0700

    p

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `uhm. cari apa?\n\ncontoh:\n${usedPrefix + command} logo`
   m.reply(wait)
-  let res = await fetch(`https://apikey-bear3.herokuapp.com/api/pinterest?query=${text}&apikey=${bearkey}`)
+  let res = await fetch(`https://saipulanuar.herokuapp.com/api/pinterest?query=${text}&apikey=${bearkey}`)
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
   if (!json.status) throw json
