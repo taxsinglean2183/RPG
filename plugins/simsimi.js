a6df8b326201b499623c7290b2f601a6975dab25
taxsinglean2183 <102736242+taxsinglean2183@users.noreply.github.com>
2022-03-31 19:11:13 +0700

    p

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} hai`
-  let res = await fetch(`https://apikey-bear3.herokuapp.com/api/f/simi2?apikey=${bearkey}&query=${text}`)
+  let res = await fetch(`https://saipulanuar.herokuapp.com/api/f/simi2?apikey=${bearkey}&query=${text}`)
   if (!res.ok) throw eror
   let json = await res.json()
   if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin jadi gatau t_t custom pesannya di https://simsimi.com/teach')
