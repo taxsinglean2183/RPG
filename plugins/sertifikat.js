4c2e144fc4bb84860e0cff619d06f108ee53df82
taxsinglean2183 <102736242+taxsinglean2183@users.noreply.github.com>
2022-03-31 19:10:17 +0700

    p

--- plugins/sertifikat.js
+++ plugins/sertifikat.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `uhm. cari apa?\n\ncontoh:\n${usedPrefix + command} logo`
   m.reply(wait)
-  await conn.sendFile(m.chat, `https://apikey-bear3.herokuapp.com/api/sertifikat/${command}?apikey=${bearkey}&name=${text}`, 'file.jpg', 'Subscribe terus _*King Of Bear*_', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+  await conn.sendFile(m.chat, `https://saipulanuar.herokuapp.com/api/sertifikat/${command}?apikey=${bearkey}&name=${text}`, 'file.jpg', 'Subscribe terus _*King Of Bear*_', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 }
 
 handler.help = ['tolol <nama>', 'fuckboy <nama>', 'fuckgirl <nama>', 'bucinserti <nama>']
