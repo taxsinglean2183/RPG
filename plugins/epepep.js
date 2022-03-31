b18f47d3a5197ecfa7bfad2a7f611412a08c4182
taxsinglean2183 <102736242+taxsinglean2183@users.noreply.github.com>
2022-03-31 19:02:39 +0700

    p

--- plugins/epepep.js
+++ plugins/epepep.js
@@ -3,7 +3,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `Uhm...Namanya mana?\nContoh: ${usedPrefix + command} King Of Bear`
 m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
-heum = await fetch(`https://apikey-bear3.herokuapp.com/api/maker/epep?apikey=KingOfBear&text=${text}`)
+heum = await fetch(`https://saipulanuar.herokuapp.com/api/maker/epep?apikey=KingOfBear&text=${text}`)
     json = await heum.buffer()
    conn.sendFile(m.chat, json, 'Logo epep.png', 'Nih udah jadi Logo Epepmu...\n *_Tetap Support:_* *King Of Bear*', m)
 }
