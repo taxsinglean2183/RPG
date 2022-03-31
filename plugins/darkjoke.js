936e7356c235b8818276593f1bdbae56eee82c39
taxsinglean2183 <102736242+taxsinglean2183@users.noreply.github.com>
2022-03-31 19:01:58 +0700

    p

--- plugins/darkjoke.js
+++ plugins/darkjoke.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
 m.reply(wait)
-heum = await fetch(`https://apikey-bear3.herokuapp.com/api/darkjokes?apikey=${bearkey}`)
+heum = await fetch(`https://saipulanuar.herokuapp.com/api/darkjokes?apikey=${bearkey}`)
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, kasihcaption, footer, 'Next', `${usedPrefix + command}`, m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
