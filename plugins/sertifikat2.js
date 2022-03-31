82696be538f02618077cb30e63de415b51c8e10f
taxsinglean2183 <102736242+taxsinglean2183@users.noreply.github.com>
2022-03-31 19:10:40 +0700

    p

--- plugins/sertifikat2.js
+++ plugins/sertifikat2.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let [teks, wm] = text.split`&`
   if (!text) throw `uhm. cari apa?\n\ncontoh:\n${usedPrefix + command} bear&athena`
   m.reply(wait)
-  await conn.sendFile(m.chat, `https://apikey-bear3.herokuapp.com/api/sertifikat/pacarserti?apikey=${bearkey}&name1=${text}&name2=${wm}`, 'file.jpg', 'Subscribe terus _*King Of Bear*_', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+  await conn.sendFile(m.chat, `https://saipulanuar.herokuapp.com/api/sertifikat/pacarserti?apikey=${bearkey}&name1=${text}&name2=${wm}`, 'file.jpg', 'Subscribe terus _*King Of Bear*_', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 }
 
 handler.help = ['pacarserti <nama>&<nama>', 'sertipacar <nama>&<nama>']
