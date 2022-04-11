f0ab388cc3f7a4ce29063be47449a3352037b6e1
taxsinglean2183 <102736242+taxsinglean2183@users.noreply.github.com>
2022-04-12 01:25:20 +0800

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,26 +1,26 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `┏━━━ꕥ〔 *Beli Bot* 〕ꕥ━⬣
-┃➥ *1 Bulan:* Rp 15.000,00
-┃➥ *Permanen:* Rp 25.000,00
-┃➥ *Premium:* Rp 10.000,00
-┃➥ *Permanen+SC:* Rp 100.000,00
-┃➥ *Jasa Run:* Rp 15.000,00
-┃➥ *Edit Sc/Nambah Fitur Bot*
-┃➥ *1 Fitur:* Rp 5.000.00 No Apikey
-┃➥ *1 Fitur:* Rp 8.000.00 + Apikey
-┃➥ *Beli Apikey:* Rp 20.000.00 = 1 Bulan
-┃➥ *Sc Bot:* https://youtu.be/Sgb5BVOW66Y
+┃➥ *2 Minggu:* RM 5.00
+┃➥ *Permanent:* RM 10.00
+┃➥ *Premium:* RM 15.00
+┃➥ *Jasa Buat Bot:* RM 20.00
+┃➥ *Jasa Run:* RM 5.00
+┃➥ *Edit Sc/tambah Fitur Bot*
+┃➥ *1 Fitur:* RM 5.00 No Apikey
+┃➥ *1 Fitur:* RM 8.00 + Apikey
+┃➥ *Beli Apikey:* RM 15.00 = 1 Bulan
+┃➥ *Sc Bot:* wa.me/60108026373
 ┗━━━ꕥ
 ┏━━━ꕥ〔 *PEMBAYARAN* 〕ꕥ━⬣
-┃➥ Gopay, Dana, Dan Ovo
+┃➥ Touch n' Go , Bankin
 ┃
 ┃✾ *Tertarik Untuk Beli Bot Ini?*
-┃➥Ketuk Tombol Di Bawah Ya
+┃➥Ketik Button Di Bawah Ya
 ┃
-┃©2022 King Of Bear
-┃Scrip original by Nurutomo
-┗━━━ꕥ〔 *${namabot}* 〕ꕥ━⬣`.trim(), footer, 'Dana', '#viadana', 'Owner', '#owner', 'List Menu', '#menu', m)
+┃©2022 Synxstore
+┃Scrip original by Synxbotz
+┗━━━ꕥ〔 *${namabot}* 〕ꕥ━⬣`.trim(), footer, 'Tng', '#viatng', 'Owner', '#owner', 'List Menu', '#menu', m)
 }
 
 handler.command = /^sewa(bot)?$/i
